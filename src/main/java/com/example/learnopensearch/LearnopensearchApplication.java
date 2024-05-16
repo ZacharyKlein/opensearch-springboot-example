a6bb4f65228e36add1d477e799aeead98d42fe87
Zachary Klein <zak@silver-chalice.com>
2024-05-16 13:39:06 -0500

    initial commit

---
+++ src/main/java/com/example/learnopensearch/LearnopensearchApplication.java
@@ -0,0 +1,13 @@
+package com.example.learnopensearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnopensearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnopensearchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/learnopensearch/LearnopensearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.learnopensearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnopensearchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
